a4f8354a5e0672c1b7cc4bbbb62717d63e70ef34
live0706 <158266584+live0706@users.noreply.github.com>
2025-09-30 10:15:33 +0200

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,18 +1,25 @@
-#include "./../../include/calculator.h"
+#include "./../../include/calculatrice.h"
 
 double _add(double a, double b) {
-	return a+b;
+    return a + b;
 }
 
 double _sub(double a, double b) {
-	return a-b;
+    return a - b;
 }
 
 double _mul(double a, double b) {
-	return a*b;
+    return a * b;
 }
 
 double _div(double a, double b) {
-	return a/b;
+    if (b == 0) {
+        fprintf(stderr, "Erreur : division par zéro !\n");
+        return 0;
+    }
+    return a / b;
 }
-	
+
+double _car(double a) {
+    return a * a;
+}
