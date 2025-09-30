8b46daf224948f20845cd40e4e99192f7a1f18f7
live0706 <158266584+live0706@users.noreply.github.com>
2025-09-30 10:34:12 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./../include/calculatrice.h"
+#include "./../include/calculator.h"
 
 int main(int argc, char *argv[]) {
     if (argc == 4) {
