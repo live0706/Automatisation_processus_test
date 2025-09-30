7e9514bc853a799afbb404db7736eb465baf6c97
live0706 <158266584+live0706@users.noreply.github.com>
2025-09-30 10:14:00 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./../include/calculator.h"
+#include "./../include/calculatrice.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
+int main(int argc, char *argv[]) {
+    if (argc == 4) {
+        char* op = argv[1];
+        char* a = argv[2];
+        char* b = argv[3];
+        double r = 0;
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else {
+            printf("Erreur de parametres\n");
+        }
+    }
+    else if (argc == 3) {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
+
+        if (strcmp(op, "car") == 0) {
+            r = _car(atof(a));
+            printf("%lf\n", r);
+        }
+        else {
+            printf("Erreur de parametres\n");
+        }
+    }
+    else {
+        printf("Erreur de parametres\n");
+    }
+    return 0;
 }
-
-		
